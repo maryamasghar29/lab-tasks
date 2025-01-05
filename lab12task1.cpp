be6df33f5cb22cf9b187e3f9b94b2d90eba6c1e0
maryamasghar29 <maryamasghar615@gmail.com>
2025-01-04 22:24:40 -0700

    Add files via upload

---
+++ lab12task1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"enter your  number"<<endl;
+    cin>>number;
+    if(number%2==1){
+        cout<<"number is odd"<<endl;
+    }
+    else{
+        cout<<"number is even"<<endl;
+    }
+    return 0;
+}
