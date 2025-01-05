852ef0daa4dc422b3dbe7be2b6bfd92a2da7bfff
maryamasghar29 <maryamasghar615@gmail.com>
2025-01-04 22:20:24 -0700

    Add files via upload

---
+++ question2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number1;
+    cout<<"enter your first number"<<endl;
+    cin>>number1;
+    int number2;
+    cout<<"enter your second number"<<endl;
+    cin>>number2;
+    int sum=number1+number2;
+    cout<<sum;
+    return 0;
+
+}
